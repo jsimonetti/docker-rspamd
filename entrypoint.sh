f97317c3a69724c0a0749e8498a8b95f0771d2a0
Jeroen Simonetti <jeroen@simonetti.nl>
2019-06-07 14:11:34 +0200

    Add dnsserver option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 set -e
 
 RSPAMD_CONTROLLER_PASSWORD="${RSPAMD_CONTROLLER_PASSWORD:-q1}"
+DNSSERVER="${DNSSERVER:-dnsserver:53:1}"
 
 if [ "$1" = 'rspamd' ]; then
     echo 'password = "'$RSPAMD_CONTROLLER_PASSWORD'";' > /etc/rspamd/local.d/worker-controller.inc
     echo 'enable_password = "'$RSPAMD_CONTROLLER_PASSWORD'";' >> /etc/rspamd/local.d/worker-controller.inc
+    echo -e 'dns {\n nameserver = ["'$DNSSERVER'"];\n}' >> /etc/rspamd/override.d/options.inc
     exec "$@"
 fi
 
