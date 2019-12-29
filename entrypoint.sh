6f2983f45a59f3721de3ad903c8754a75f3ad427
Jeroen Simonetti <jeroen@simonetti.nl>
2019-12-29 12:33:03 +0100

    Optional dnsserver
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,14 @@
 set -e
 
 RSPAMD_CONTROLLER_PASSWORD="${RSPAMD_CONTROLLER_PASSWORD:-q1}"
-DNSSERVER="${DNSSERVER:-dnsserver:53:1}"
+DNSSERVER="${DNSSERVER:-X}"
 
 if [ "$1" = 'rspamd' ]; then
     echo 'password = "'$RSPAMD_CONTROLLER_PASSWORD'";' > /etc/rspamd/local.d/worker-controller.inc
     echo 'enable_password = "'$RSPAMD_CONTROLLER_PASSWORD'";' >> /etc/rspamd/local.d/worker-controller.inc
-    echo -e 'dns {\n nameserver = ["'$DNSSERVER'"];\n}' >> /etc/rspamd/override.d/options.inc
+    if [ "x${DNSSERVER}" != "xX" ]; then
+      echo -e 'dns {\n nameserver = ["'$DNSSERVER'"];\n}' >> /etc/rspamd/override.d/options.inc
+    fi
     exec "$@"
 fi
 
